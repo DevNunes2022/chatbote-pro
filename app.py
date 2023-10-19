018c563a55e3e4238ba4d725f87fc0adb4bb36f4
DevNunes2022 <99822873+DevNunes2022@users.noreply.github.com>
2023-10-19 07:06:39 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite um número:"))
+n2 = int(input("Digite mais um número:"))
+s = n1 + n2
+print("A soma vale:",s)
+
+#tipos primitivos: int, float, bool, str.
+#Int = Pode receber 7,-4,0,9875.
+#float = Pode receber 7,-4.9,98.75.
+#bool = Pode receber true ou false
+#strings = Pode receber valores de texto como "Anderson"
+
